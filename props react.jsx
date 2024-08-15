7c1befaa02944c39554b0db23ebf0ccf01b9d74e
Bhavishya tiwari <85561955+Bhavishya-tiwari@users.noreply.github.com>
2024-08-15 17:35:45 +0530

    Create props react.jsx

---
+++ props react.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card(props) {
+  console.log(props.username);
+  return (
+    <div className="relative h-[400px] w-[300px] rounded-md ">
+      // card matter
+    </div>
+  )
+}
+
+export default Card
